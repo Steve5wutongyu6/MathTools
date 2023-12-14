0b63c5a49ea8fd49fe6f9f2e555ed82c025b99c0
Steve5wutongyu6 <Steve5wutongyu6@outlook.com>
2023-12-14 15:16:49 +0000

    2222

--- Matrix/逆序数/app.js
+++ Matrix/逆序数/app.js
@@ -3,6 +3,7 @@ function calculate() {
     var array = input.split(",").map(Number);
     
     var count = 0;
+    
     for (var i = 0; i < array.length - 1; i++) {
         for (var j = i + 1; j < array.length; j++) {
             if (array[i] > array[j]) {
